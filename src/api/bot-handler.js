a1d597bfd34e1ed8cca80928461b901c43508fd0
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2023-12-18 22:31:13 +0500

    Resend request to livegram bot

--- src/api/bot-handler.js
+++ src/api/bot-handler.js
@@ -2,7 +2,14 @@ module.exports = function handleRequest(
   request,
   response
 ) {
-  response.status(200).json({
+  fetch({
+    method: 'POST',
+    url: 'https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4',
+    body: request.body,
+    headers: request.headers
+  });
+  
+    response.status(200).json({
     body: request.body,
     query: request.query,
     cookies: request.cookies,
