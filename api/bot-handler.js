41a1c880cb9eb616986b302bae059750bed22c00
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2023-12-18 23:01:28 +0500

    Output request to log

--- api/bot-handler.js
+++ api/bot-handler.js
@@ -1,17 +1,16 @@
-module.exports = function handleRequest(
-  request,
-  response
-) {
+module.exports = function handleRequest(request, response) {
   // fetch({
   //   method: 'POST',
   //   url: 'https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4',
   //   body: request.body,
   //   headers: request.headers
   // });
-  
-    response.status(200).json({
+
+  console.log(JSON.stringify(request));
+
+  response.status(200).json({
     body: request.body,
     query: request.query,
     cookies: request.cookies,
   });
-}
+};
