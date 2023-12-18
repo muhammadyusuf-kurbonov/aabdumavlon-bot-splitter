099846c1b2611cd8413abbb381303dfb6b3e3012
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2023-12-18 23:11:26 +0500

    Catch errors

--- api/bot-handler.js
+++ api/bot-handler.js
@@ -1,12 +1,17 @@
+/**
+ *
+ * @param {import('@vercel/node').VercelRequest} request
+ * @param {import('@vercel/node').VercelResponse} response
+ */
 module.exports = function handleRequest(request, response) {
-  // fetch({
-  //   method: 'POST',
-  //   url: 'https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4',
-  //   body: request.body,
-  //   headers: request.headers
-  // });
+  fetch({
+    method: "POST",
+    url: "https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4",
+    body: request.body,
+    headers: request.headers,
+  }).catch((error) => console.warn("Livegram redirect failed", error));
 
-  console.log((request));
+  console.log(request);
 
   response.status(200).json({
     body: request.body,
