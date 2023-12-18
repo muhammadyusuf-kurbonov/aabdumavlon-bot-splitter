867981ed136e5ea0230ac8eba05ba18fc22a4484
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2023-12-18 23:00:00 +0500

    Update bot-handler

--- api/bot-handler.js
+++ api/bot-handler.js
@@ -2,12 +2,12 @@ module.exports = function handleRequest(
   request,
   response
 ) {
-  fetch({
-    method: 'POST',
-    url: 'https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4',
-    body: request.body,
-    headers: request.headers
-  });
+  // fetch({
+  //   method: 'POST',
+  //   url: 'https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4',
+  //   body: request.body,
+  //   headers: request.headers
+  // });
   
     response.status(200).json({
     body: request.body,
