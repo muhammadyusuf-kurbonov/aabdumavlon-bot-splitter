4b4b1735b7aef72c97cfc0e9669e81258100b39b
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2023-12-19 22:47:18 +0500

    Use const var for request body

--- api/bot-handler.js
+++ api/bot-handler.js
@@ -11,11 +11,15 @@ module.exports = async function handleRequest(request, response) {
   });
   const axiosInstance = new axios.Axios({
     httpAgent: httpsAgent,
-  })
+  });
+
+  const requestBody = request.body;
+  console.log("🚀 ~ file: bot-handler.js:17 ~ handleRequest ~ requestBody:", requestBody, 'type: ', typeof requestBody);
+
   await Promise.all([
     axiosInstance.post(
       "https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4",
-      request.body,
+      requestBody,
       {
         headers: request.headers,
       }
@@ -24,7 +28,7 @@ module.exports = async function handleRequest(request, response) {
       .catch((error) => console.warn("Livegram redirect failed", error)),
     axiosInstance.post(
       "https://amojo.amocrm.ru/~external/hooks/telegram?t=6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4&",
-      request.body,
+      requestBody,
       {
         headers: request.headers,
       }
@@ -32,8 +36,6 @@ module.exports = async function handleRequest(request, response) {
       .then((res) => console.log("AMOCRM response", res))
       .catch((error) => console.warn("AMOCRM redirect failed", error))
     ]);
-
-  console.log(request.body);
 
   response.status(200).json({
     body: request.body,
