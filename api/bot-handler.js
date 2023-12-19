dc6abd133eebef53fb18c495e701a834fecc64d0
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2023-12-19 16:50:47 +0500

    Add AMOCRM

--- api/bot-handler.js
+++ api/bot-handler.js
@@ -8,9 +8,14 @@ module.exports = function handleRequest(request, response) {
     method: "POST",
     body: request.body,
     headers: request.headers,
-  }).catch((error) => console.warn("Livegram redirect failed", error));
+  }).then((res) => console.log('Livegram response', res.body)).catch((error) => console.warn("Livegram redirect failed", error));
+  fetch("https://amojo.amocrm.ru/~external/hooks/telegram?t=6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4&", {
+    method: "POST",
+    body: request.body,
+    headers: request.headers,
+  }).then((res) => console.log('AMOCRM response', res.body)).catch((error) => console.warn("AMOCRM redirect failed", error));
 
-  console.log(request);
+  console.log(request.body);
 
   response.status(200).json({
     body: request.body,
