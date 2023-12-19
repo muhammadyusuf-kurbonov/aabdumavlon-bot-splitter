a314a139f31ab4d17851d10e0da9046819ebcdd0
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2023-12-19 16:56:03 +0500

    Use async handlaer

--- api/bot-handler.js
+++ api/bot-handler.js
@@ -3,17 +3,29 @@
  * @param {import('@vercel/node').VercelRequest} request
  * @param {import('@vercel/node').VercelResponse} response
  */
-module.exports = function handleRequest(request, response) {
-  fetch("https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4", {
-    method: "POST",
-    body: request.body,
-    headers: request.headers,
-  }).then((res) => console.log('Livegram response', res.body)).catch((error) => console.warn("Livegram redirect failed", error));
-  fetch("https://amojo.amocrm.ru/~external/hooks/telegram?t=6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4&", {
-    method: "POST",
-    body: request.body,
-    headers: request.headers,
-  }).then((res) => console.log('AMOCRM response', res.body)).catch((error) => console.warn("AMOCRM redirect failed", error));
+module.exports = async function handleRequest(request, response) {
+  await Promise.all(
+    fetch(
+      "https://livegram.io/_run/bot/6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4",
+      {
+        method: "POST",
+        body: request.body,
+        headers: request.headers,
+      }
+    )
+      .then((res) => console.log("Livegram response", res.body))
+      .catch((error) => console.warn("Livegram redirect failed", error)),
+    fetch(
+      "https://amojo.amocrm.ru/~external/hooks/telegram?t=6880547331:AAGMVOokNyBLtMLqsRRP3HKGp-fRFqPM5i4&",
+      {
+        method: "POST",
+        body: request.body,
+        headers: request.headers,
+      }
+    )
+      .then((res) => console.log("AMOCRM response", res.body))
+      .catch((error) => console.warn("AMOCRM redirect failed", error))
+  );
 
   console.log(request.body);
 
